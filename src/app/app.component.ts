1ee5dbd79f35c6d1dde26a785b06c8567af745dd
Jakub Nowikowski <kubanowikowski@gmail.com>
2019-08-06 19:32:06 +0200

    login with fake backend server added

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+// import { Component } from "@angular/core";
+
+// @Component({
+//   selector: "pm-root",
+//   template: `
+//     <nav class="navbar-expand nabar-light bg-light">
+//       <ul class="nav nav-pills">
+//         <li><a class="nav-link" [routerLink]="['/login']">Log in</a></li>
+//         <li><a class="nav-link" [routerLink]="['/home']">Home</a></li>
+//         <li><a class="nav-link" [routerLink]="['/products']">My profile</a></li>
+//         <li><a class="nav-link" [routerLink]="['/products']">Log out</a></li>
+//       </ul>
+//     </nav>
+//     <div class="container">
+//       <router-outlet></router-outlet>
+//     </div>
+//   `
+// })
+// export class AppComponent {
+//   pageTitle: string = "Acme Product Management";
+// }
+
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { AuthenticationService } from "./_services";
+import { User } from "./_models";
+
+@Component({ selector: "app", templateUrl: "app.component.html" })
+export class AppComponent {
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      x => (this.currentUser = x)
+    );
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(["/login"]);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,91 @@
+// import { BrowserModule } from "@angular/platform-browser";
+// import { NgModule } from "@angular/core";
+// import { FormsModule } from "@angular/forms";
+// import { HttpClientModule , HTTP_INTERCEPTORS } from "@angular/common/http";
+// import { RouterModule } from "@angular/router";
+// import { ReactiveFormsModule } from '@angular/forms';
+
+// import { AppComponent } from "./app.component";
+// import { ProductListComponent } from "./products/product-list.component";
+// import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
+// import { StarComponent } from "./shared/star.component";
+// import { ProductDetailComponent } from "./products/product-detail.component";
+// import { HomeComponent } from "./home/home.component";
+// import { LoginComponent } from "./login/login.component";
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ProductListComponent,
+//     ConvertToSpaces,
+//     StarComponent,
+//     ProductDetailComponent,
+//     HomeComponent,
+//     LoginComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     HttpClientModule,
+//     RouterModule.forRoot([
+//       { path: "products", component: ProductListComponent },
+//       { path: "products/:id", component: ProductDetailComponent },
+//       { path: "login", component: LoginComponent },
+//       { path: "home", component: HomeComponent },
+//       { path: "", redirectTo: "login", pathMatch: "full" },
+//       { path: "**", redirectTo: "loign", pathMatch: "full" }
+//     ])
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule {}
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+// used to create fake backend
+import { fakeBackendProvider } from "./_helpers";
+
+import { appRoutingModule } from "./app.routing";
+import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home";
+import { LoginComponent } from "./login";
+import { RegisterComponent } from "./register";
+import { AlertComponent } from "./_components";
+import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AlertComponent,
+    ProductListComponent,
+    ConvertToSpaces,
+    StarComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
