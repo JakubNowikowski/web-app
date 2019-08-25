88ddbe711b5dfb33de1499d416a0f88c8f16e3cb
Jakub Nowikowski <kubanowikowski@gmail.com>
2019-08-25 18:11:48 +0200

    Adding posts added

---
+++ src/app/_services/content.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Post } from "../_models";
+
+@Injectable({ providedIn: "root" })
+export class ContentService {
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http.get<Post[]>(`https://localhost:44380/api/posts`);
+  }
+
+  addPost(post: Post) {
+    return this.http.post(`https://localhost:44380/api/posts`, post);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`/users/${id}`);
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,32 @@
 import { Component } from "@angular/core";
 import { first } from "rxjs/operators";
 
-import { User } from "../_models";
-import { UserService, AuthenticationService } from "../_services";
+import { User, Post } from "../_models";
+import {
+  UserService,
+  AuthenticationService,
+  ContentService
+} from "../_services";
 
 @Component({
   templateUrl: "./home.component.html"
 })
 export class HomeComponent {
   public pageTitle = "Home";
-
+  currentUser: User;
   users: User[] = [];
+  posts: Post[] = [];
+  post: Post;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private contentService: ContentService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      x => (this.currentUser = x)
+    );
+  }
 
   ngOnInit() {
     this.userService
@@ -20,6 +34,48 @@ export class HomeComponent {
       .pipe(first())
       .subscribe(users => {
         this.users = users;
+      });
+
+    this.contentService
+      .getAll()
+      .pipe(first())
+      .subscribe(posts => {
+        this.posts = posts;
+      });
+  }
+
+  _inputContent: string;
+  get inputContent(): string {
+    return this._inputContent;
+  }
+  set inputContent(value: string) {
+    this._inputContent = value;
+  }
+
+  addPost(): void {
+    this.post = {
+      username: this.currentUser.username,
+      content: this.inputContent
+    };
+    this.contentService
+      .addPost(this.post)
+      .pipe(first())
+      .subscribe(
+        data => {
+          // this.alertService.success("Registration successful", true);
+          // this.router.navigate(["/login"]);
+        },
+        error => {
+          // this.alertService.error(error);
+          // this.loading = false;
+        }
+      );
+
+    this.contentService
+      .getAll()
+      .pipe(first())
+      .subscribe(posts => {
+        this.posts = posts;
       });
   }
 }
